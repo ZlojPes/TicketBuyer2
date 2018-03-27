1ddc4d6f011123db46064ebc87334fb79ba835ac
ospavliuk <asp4rever@gmail.com>
2018-03-27 15:46:54 +0300

    add

--- src/main/java/com/ospavliuk/ticketbuyer2/controller/Controller.java
+++ src/main/java/com/ospavliuk/ticketbuyer2/controller/Controller.java
@@ -3,5 +3,12 @@ package com.ospavliuk.ticketbuyer2.controller;
 public interface Controller {
     void fromFieldTyping();
     void toFieldTyping();
+    void changeDirection();
+    void dateChanged();
+    void trainNumberSelected();
+    void wagonTypeSelected();
+    void authSelected();
+    void startPressed();
+
 
 }
