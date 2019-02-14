84e8059b6975725f17bfe40cc7ae3baa3c9e8764
Alex <asp4rever@gmail.com>
2019-02-14 22:32:19 +0200

    beta2

--- src/main/java/com/ospavliuk/ticketbuyer2/model/jsonparser/placelist/Places.java
+++ src/main/java/com/ospavliuk/ticketbuyer2/model/jsonparser/placelist/Places.java
@@ -1,13 +1,17 @@
 package com.ospavliuk.ticketbuyer2.model.jsonparser.placelist;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 
 import java.util.List;
 
-@SuppressWarnings("WeakerAccess")
 public class Places {
-    @Getter
     @JsonProperty("А")
     private List<String> placeList;
+
+    @JsonProperty("Б")
+    private List<String> placeList2;
+
+    public List<String> getPlaceList() {
+        return placeList != null ? placeList : placeList2;
+    }
 }
